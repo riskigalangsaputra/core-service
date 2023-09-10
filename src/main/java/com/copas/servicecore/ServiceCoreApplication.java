4daa19241c2de82d533b5f153105261b0f030089
riskigalangsaputra <riskigalangsaputra@gmail.com>
2023-09-10 15:08:53 +0700

    init

---
+++ src/main/java/com/copas/servicecore/ServiceCoreApplication.java
@@ -0,0 +1,13 @@
+package com.copas.servicecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/copas/servicecore/ServiceCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copas.servicecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
